e4266543453a4bfac314024c43b08b319ce73553
mfolino <matefolino@gmail.com>
2022-08-01 19:24:29 -0300

    cheques

--- presentarSPRINT5/transferencias.py
+++ presentarSPRINT5/transferencias.py
@@ -16,10 +16,23 @@ def menu():
 
 
 def black():
-    
+    f = open('cheques.html', 'w')
+    html_template = """<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>IT_BANK</title>
+<link rel="icon" type="image/x-icon" href="imags/N-ITBANK.jpg">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="css/prueba.css">
+</head>
+<body>
+"""
+    f.write(html_template)
     import json
 
-    datos = open("black.json", "r")
+    datos = open("presentarSprint5/black.json", "r")
     cuentaBlack = json.load(datos)
 
     for transferencias in cuentaBlack['transacciones']:
@@ -28,15 +41,38 @@ def black():
     
         if transferencias['estado'] == "ACEPTADA":
             
-            print("\n")
-            print("Estado: ", transferencias['estado'])
-            print("Tipo: ", transferencias['tipo'])
-            print("Monto: ", transferencias['monto'])
-            print("Fecha: ", transferencias['fecha'])
-            print("Saldo en cuenta: ", transferencias['saldoEnCuenta'])
-            print("Cupo diario restante: ", transferencias['cupoDiarioRestante'])
-            print("\n")
-    
+            # print("\n")
+            # print("Estado: ", transferencias['estado'])
+            # print("Tipo: ", transferencias['tipo'])
+            # print("Monto: ", transferencias['monto'])
+            # print("Fecha: ", transferencias['fecha'])
+            # print("Saldo en cuenta: ", transferencias['saldoEnCuenta'])
+            # print("Cupo diario restante: ", transferencias['cupoDiarioRestante'])
+            # print("\n")
+            
+            valor_html = "<p>"
+            valor_html = valor_html + "Estado: " + transferencias['estado'] + "</br>"
+            f.write(valor_html)
+            valor_html = "</p>"
+            f.write(valor_html)
+            valor_html = "<p>"
+            valor_html = valor_html + "Tipo: " + transferencias['tipo'] + "</br>"
+            f.write(valor_html)
+            valor_html = "</p>"
+            f.write(valor_html)
+            valor_html = "<p>"
+            valor_html = valor_html + "Monto: " + f"transferencias['monto']" + "</br>"
+            f.write(valor_html)
+            valor_html = "</p>"
+            f.write(valor_html)
+            valor_html = "<p>"
+            valor_html = valor_html + "Fecha: " + transferencias['fecha'] + "</br>"
+            f.write(valor_html)
+            valor_html = "</p>"
+            f.write(valor_html)
+            valor_html = "<p>"
+            
+
         elif transferencias['estado'] == "RECHAZADA" and transferencias['tipo'] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO" and transferencias['monto'] > transferencias['cupoDiarioRestante'] :
 
             
@@ -92,6 +128,12 @@ def black():
             print("Saldo en cuenta: ", transferencias['saldoEnCuenta'])
             print("Cupo diario restante: ", transferencias['cupoDiarioRestante'])
             print("\n")  
+    html_pie_template="""
+    </body>
+    </html>
+    """
+    f.write(html_pie_template)
+    f.close()
 
 def classic():
     
@@ -99,7 +141,7 @@ def classic():
     
     import json
 
-    datos = open("classic.json", "r")
+    datos = open("presentarSprint5/classic.json", "r")
     cuentaClassic = json.load(datos)
 
     for transferencias in cuentaClassic['transacciones']:
@@ -182,7 +224,7 @@ def classic():
 def gold(): 
     import json
 
-    datos = open("gold.json", "r")
+    datos = open("presentarSprint5/gold.json", "r")
     cuentaGold = json.load(datos)
 
     for transferencias in cuentaGold['transacciones']:
@@ -254,4 +296,30 @@ def gold():
             print("Cupo diario restante: ", transferencias['cupoDiarioRestante'])
             print("\n") 
 
+
+
 menu()
+
+# f = open('cheques.html', 'w')
+
+# html_template = """<html>
+# <head>
+# <meta charset="UTF-8">
+# <meta http-equiv="X-UA-Compatible" content="IE=edge">
+# <meta name="viewport" content="width=device-width, initial-scale=1.0">
+# <title>IT_BANK</title>
+# <link rel="icon" type="image/x-icon" href="imags/N-ITBANK.jpg">
+# <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+# <link rel="stylesheet" href="css/prueba.css">
+# </head>
+# <body>
+# <main>
+
+# </main>
+# </body>
+# </html>
+# """
+
+# f.write(html_template)
+# f.write(valor_html)
+# f.close()
